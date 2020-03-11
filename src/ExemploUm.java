a6e5f2147927e86df16f42f92422a4a82863c10a
Aluno ETEC HAS <marianeallencar@outlook.com>
2020-03-11 20:32:22 -0300

    retorne false ou true

---
+++ src/ExemploUm.java
@@ -0,0 +1,14 @@
+public class ExemploUm 
+{
+	public static boolean ehDiaSemana(String dia)
+	{
+		if(dia == "Domingo" || dia == "Sabado")
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
