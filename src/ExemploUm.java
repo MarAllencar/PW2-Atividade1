a82b76640760393745f07836c7abeb45f0354950
Aluno ETEC HAS <marianeallencar@outlook.com>
2020-03-11 21:35:04 -0300

    complementação do codigo

--- src/ExemploUm.java
+++ src/ExemploUm.java
@@ -2,7 +2,7 @@ public class ExemploUm
 {
 	public static boolean ehDiaSemana(String dia)
 	{
-		if(dia == "Domingo" || dia == "Sabado")
+		if(dia.equalsIgnoreCase("Domingo") || dia.equalsIgnoreCase("Sabado"))
 		{
 			return false;
 		}
@@ -10,5 +10,11 @@ public class ExemploUm
 		{
 			return true;
 		}
+	}
+
+	public static void main(String[] args) 
+	{
+		boolean diaValido = ehDiaSemana("Domingo");
+		System.out.println("é dia semana: " + diaValido);
 	}
 }
